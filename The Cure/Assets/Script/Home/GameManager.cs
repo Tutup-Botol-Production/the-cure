d6bc5b110faa70ba376cf9a41fae5a3148ff1469
Mulia Firmansyah Arief <muliafirmansyah16@gmail.com>
2023-01-29 15:24:23 +0700

    fear: create menu in game

--- The Cure/Assets/Script/Home/GameManager.cs
+++ The Cure/Assets/Script/Home/GameManager.cs
@@ -6,13 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject itemInteract = null;
+    [HideInInspector] public GameObject itemInteract = null;
+    [HideInInspector] public bool isMenuOpen = false;
+
+    [SerializeField] private GameObject subMenu;
 
     public void OnInteractBtnClick()
     {
         if (itemInteract != null)
         {
             Destroy(itemInteract);
+        }
+    }
+
+    public void OnMenuBtnClick()
+    {
+        if (isMenuOpen)
+        {
+            subMenu.SetActive(false);
+            isMenuOpen = false;
+        } else
+        {
+            subMenu.SetActive(true);
+            isMenuOpen = true;
         }
     }
 
