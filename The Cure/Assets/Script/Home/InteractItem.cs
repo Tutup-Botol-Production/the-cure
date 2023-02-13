0aedfedfe2c9db6c785c6c4df496cce5e29b4764
Toto Jati Wijayanto <97005196+totiw@users.noreply.github.com>
2023-02-13 21:39:31 +0700

    Refactor file & Implement trigger system
    feat: implement trigger system when all objective collected
    refactor: delete unnecessary file
    refactor: refactor code in game manager when button interact click

--- The Cure/Assets/Script/Home/InteractItem.cs
+++ The Cure/Assets/Script/Home/InteractItem.cs
@@ -5,6 +5,26 @@ public class InteractItem : MonoBehaviour
     [SerializeField] private GameObject interactBtn;
     [SerializeField] private GameObject gameManager;
 
+    private int bag;
+
+    private void Awake()
+    {
+        bag = PlayerPrefs.GetInt("bagTaken");
+    }
+
+    private void Start()
+    {
+        if (bag == 1 && gameObject.CompareTag("bag"))
+        {
+            ClearObject();
+        }
+    }
+
+    public void ClearObject()
+    {
+        Destroy(gameObject);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
